4cbaea9da09251d6f74f06e20c4b9b262f09fe46
PhilippeDubosc <duboscph@gmail.com>
2015-11-01 23:16:04 +0100

    Connexion to MongoLAB

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var express = require('express');
-var mongoClient = require('mongodb').MongoClient
+var mongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 var cool = require('cool-ascii-faces');
 var app = express();
 
 var mongoUrl = 'mongodb://myweekly:gopotodo@ds049084.mongolab.com:49084/heroku_kl782g06';
-mongoClient.connect(mongoUrl, function (err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    db.close();
-});
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -25,9 +22,30 @@ app.get('/cool', function (request, response) {
     response.send(cool());
 });
 
-app.get('/weeklynewspaper', function (request, response) {
+app.get('/weeklynewspapers', function (request, response) {
+    mongoClient.connect(mongoUrl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        var findDocuments = function(db/*, callback*/) {
+            // Get the documents collection
+            var collection = db.collection('weeklynewspaper');
+            console.log("find");
+            // Find some documents
+            collection.find({}).toArray(function(err, docs) {
+                console.log("validate");
+                assert.equal(err, null);
+                assert.equal(1, docs.length);
 
-    response.send(cool());
+                console.log("Found the following records");
+                console.dir(docs);
+                //callback(docs);
+            });
+        }
+        console.log("call");
+        findDocuments(db);
+        db.close();
+    });
+    response.send("ok");
 });
 
 
