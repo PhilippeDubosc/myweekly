d2e5e93287131b64ddf1ce38ed3b706040b1af0c
PhilippeDubosc <duboscph@gmail.com>
2015-11-01 23:01:11 +0100

    Connexion to MongoLAB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var mongoClient = require('mongodb').MongoClient
 var cool = require('cool-ascii-faces');
 var app = express();
+
+var mongoUrl = 'mongodb://myweekly:gopotodo@ds049084.mongolab.com:49084/heroku_kl782g06';
+mongoClient.connect(mongoUrl, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.close();
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,14 +18,21 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/cool', function (request, response) {
+    response.send(cool());
 });
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+app.get('/weeklynewspaper', function (request, response) {
+
+    response.send(cool());
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
